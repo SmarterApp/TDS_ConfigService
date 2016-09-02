5c9a78cf787d137a77693272bd02ffccd1c8efc9
Jeff Johnson <jeffjohnson9046@hotmail.com>
2016-09-01 17:00:09 -0700

    TDS-328: Add NotFoundException and ClientSystemFlag unit tests

---
+++ service/src/main/java/tds/config/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package tds.config.exceptions;
+
+
+public class NotFoundException extends Exception {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
--- service/src/main/java/tds/config/web/resources/ClientTestPropertyResource.java
+++ service/src/main/java/tds/config/web/resources/ClientTestPropertyResource.java
@@ -3,6 +3,7 @@ package tds.config.web.resources;
 import org.springframework.hateoas.ResourceSupport;
 import tds.config.ClientTestProperty;
 import tds.config.endpoints.ConfigController;
+import tds.config.exceptions.NotFoundException;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -13,8 +14,8 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 public class ClientTestPropertyResource extends ResourceSupport {
     private final ClientTestProperty clientTestProperty;
 
-    // TODO:  Having to have a "throws" here is a code smell (its from the methodOn().getClientTestProperty call).  Investigate another way to build the self link.
-    public ClientTestPropertyResource(ClientTestProperty clientTestProperty) {
+    // TODO:  Having to have a "throws" here is a code smell (its from the methodOn().getClientTestProperty call, which throws a checked exception).  Investigate another way to build the self link.
+    public ClientTestPropertyResource(ClientTestProperty clientTestProperty) throws NotFoundException {
         this.clientTestProperty = clientTestProperty;
         this.add(linkTo(
                 methodOn(ConfigController.class)
