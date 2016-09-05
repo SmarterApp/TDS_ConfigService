08121a2073dfd841714184fbd46f685aa9d8f60a
Jeff Johnson <jeffjohnson9046@hotmail.com>
2016-09-05 11:33:43 -0700

    TDS-328: additional comments

--- service/src/test/resources/db/migration/beforeMigrate.sql
+++ service/src/test/resources/db/migration/beforeMigrate.sql
@@ -1,2 +1,12 @@
+/***********************************************************************************************************************
+  File: beforeMigrate.sql
+
+  Desc: Create the configs and session schemas that the Config service depends on.  This script will be executed as a
+  part of Flyway's run cycle before any of the migrations are executed.  For more details on how to hook into Flyway,
+  check here:  https://flywaydb.org/documentation/callbacks
+
+  Auth:  Jeff Johnson <jeffjohnson9046>
+
+***********************************************************************************************************************/
 CREATE SCHEMA IF NOT EXISTS configs;
 CREATE SCHEMA IF NOT EXISTS session;
