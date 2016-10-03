d4a668dfdb6cf84eb704abd6cd55fa16a2f5900b
Todd Miller <tmiller@fairwaytech.com>
2016-10-03 09:48:18 -0700

    Use new timestamp mapper utility

--- service/src/main/java/tds/config/repositories/impl/mappers/ClientSystemFlagRowMapper.java
+++ service/src/main/java/tds/config/repositories/impl/mappers/ClientSystemFlagRowMapper.java
@@ -1,12 +1,13 @@
 package tds.config.repositories.impl.mappers;
 
 import org.springframework.jdbc.core.RowMapper;
-import tds.config.ClientSystemFlag;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.Instant;
-import java.time.ZoneOffset;
+
+import tds.config.ClientSystemFlag;
+
+import static tds.common.data.mapping.ResultSetMapperUtility.mapTimeStampToInstant;
 
 /**
  * Map a {@link ClientSystemFlag} from the database to a POJO.
@@ -14,22 +15,14 @@ import java.time.ZoneOffset;
 public class ClientSystemFlagRowMapper implements RowMapper<ClientSystemFlag> {
     @Override
     public ClientSystemFlag mapRow(ResultSet rs, int i) throws SQLException {
-        Instant dateChanged = rs.getTimestamp("dateChanged") == null
-                ? null
-                : rs.getTimestamp("dateChanged").toLocalDateTime().toInstant(ZoneOffset.UTC);
-
-        Instant datePublished = rs.getTimestamp("datePublished") == null
-                ? null
-                : rs.getTimestamp("datePublished").toLocalDateTime().toInstant(ZoneOffset.UTC);
-
         return new ClientSystemFlag.Builder()
                 .withAuditObject(rs.getString("auditObject"))
                 .withClientName(rs.getString("clientName"))
                 .withIsPracticeTest(rs.getBoolean("isPracticeTest"))
                 .withIsOn(rs.getInt("isOn") == 1)
                 .withDescription(rs.getString("description"))
-                .withDateChanged(dateChanged)
-                .withDatePublished(datePublished)
+                .withDateChanged(mapTimeStampToInstant(rs, "dateChanged"))
+                .withDatePublished(mapTimeStampToInstant(rs, "datePublished"))
                 .build();
     }
 }
