4fddb9a0fadbd4c3714bc8f695f70feb7ac4fd81
Todd Miller <tmiller@fairwaytech.com>
2016-10-14 13:46:24 -0700

    Renamed the controller integration test

--- service/src/test/java/tds/config/web/endpoints/ConfigControllerIntegrationTests.java
+++ service/src/test/java/tds/config/web/endpoints/ConfigControllerIntegrationTests.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(ConfigController.class)
-public class ConfigEndpointIntegrationTests {
+public class ConfigControllerIntegrationTests {
     @Autowired
     private MockMvc http;
 
