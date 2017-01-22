f2ab5e78f3a188b0bf5fff70a6772e01cfd76a36
Todd Miller <tmiller@fairwaytech.com>
2017-01-22 10:14:25 -0800

    Migrated assessment windows and accommodations endpoints to the assessment service

--- service/src/main/java/tds/config/services/ConfigService.java
+++ service/src/main/java/tds/config/services/ConfigService.java
@@ -1,11 +1,8 @@
 package tds.config.services;
 
-import java.util.List;
 import java.util.Optional;
 
-import tds.config.AssessmentWindow;
 import tds.config.ClientSystemFlag;
-import tds.config.model.AssessmentWindowParameters;
 
 /**
  * Provide an interface for interacting with the {@code configs} database and providing configuration information.
@@ -19,12 +16,4 @@ public interface ConfigService {
      * @return The {@link Optional<ClientSystemFlag>} that matches the client and audit name.
      */
     Optional<ClientSystemFlag> findClientSystemFlag(String clientName, String type);
-
-    /**
-     * Finds the {@link tds.config.AssessmentWindow AssessmentWindow} for the given {@link tds.config.model.AssessmentWindowParameters}
-     *
-     * @param assessmentWindowParameters {@link tds.config.model.AssessmentWindowParameters} propeties for the assessment window
-     * @return list of {@link tds.config.AssessmentWindow} that fit the parameters
-     */
-    List<AssessmentWindow> findAssessmentWindows(AssessmentWindowParameters assessmentWindowParameters);
 }
--- service/src/test/java/tds/config/web/endpoints/ConfigControllerTest.java
+++ service/src/test/java/tds/config/web/endpoints/ConfigControllerTest.java
@@ -2,7 +2,6 @@ package tds.config.web.endpoints;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -10,18 +9,13 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 import tds.common.web.exceptions.NotFoundException;
-import tds.config.AssessmentWindow;
 import tds.config.ClientSystemFlag;
-import tds.config.model.AssessmentWindowParameters;
 import tds.config.services.ConfigService;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -71,71 +65,5 @@ public class ConfigControllerTest {
             .thenReturn(Optional.empty());
 
         configController.getClientSystemFlag(clientName, auditObject);
-    }
-
-    @Test
-    public void shouldFindListOfAssessmentWindows() {
-        AssessmentWindow window = new AssessmentWindow.Builder()
-            .withAssessmentKey("assessment")
-            .withWindowId("windowId")
-            .build();
-
-        when(mockConfigService.findAssessmentWindows(isA(AssessmentWindowParameters.class))).thenReturn(Collections.singletonList(window));
-
-        ResponseEntity<List<AssessmentWindow>> response = configController.findAssessmentWindows("SBAC_PT",
-            "assessment",
-            1,
-            25,
-            50,
-            75,
-            100,
-            "wid:formKey");
-
-        ArgumentCaptor<AssessmentWindowParameters> assessmentWindowParametersArgumentCaptor = ArgumentCaptor.forClass(AssessmentWindowParameters.class);
-        verify(mockConfigService).findAssessmentWindows(assessmentWindowParametersArgumentCaptor.capture());
-
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).containsExactly(window);
-
-        AssessmentWindowParameters parameters = assessmentWindowParametersArgumentCaptor.getValue();
-        assertThat(parameters.getAssessmentId()).isEqualTo("assessment");
-        assertThat(parameters.getClientName()).isEqualTo("SBAC_PT");
-        assertThat(parameters.getStudentId()).isEqualTo(1);
-        assertThat(parameters.getShiftWindowStart()).isEqualTo(25);
-        assertThat(parameters.getShiftWindowEnd()).isEqualTo(50);
-        assertThat(parameters.getShiftFormStart()).isEqualTo(75);
-        assertThat(parameters.getShiftFormEnd()).isEqualTo(100);
-        assertThat(parameters.getFormList()).isEqualTo("wid:formKey");
-    }
-
-    @Test
-    public void shouldFindListOfAssessmentWindowsWithNullArguments() {
-        AssessmentWindow window = new AssessmentWindow.Builder()
-            .build();
-
-        when(mockConfigService.findAssessmentWindows(isA(AssessmentWindowParameters.class))).thenReturn(Collections.singletonList(window));
-
-        ResponseEntity<List<AssessmentWindow>> response = configController.findAssessmentWindows("SBAC",
-            "assessment",
-            1,
-            null,
-            null,
-            null,
-            null,
-            null);
-
-        ArgumentCaptor<AssessmentWindowParameters> assessmentWindowParametersArgumentCaptor = ArgumentCaptor.forClass(AssessmentWindowParameters.class);
-        verify(mockConfigService).findAssessmentWindows(assessmentWindowParametersArgumentCaptor.capture());
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).containsExactly(window);
-
-        AssessmentWindowParameters parameters = assessmentWindowParametersArgumentCaptor.getValue();
-        assertThat(parameters.getShiftWindowStart()).isEqualTo(0);
-        assertThat(parameters.getShiftWindowEnd()).isEqualTo(0);
-        assertThat(parameters.getShiftFormStart()).isEqualTo(0);
-        assertThat(parameters.getShiftFormEnd()).isEqualTo(0);
-        assertThat(parameters.getFormList()).isNull();
     }
 }
