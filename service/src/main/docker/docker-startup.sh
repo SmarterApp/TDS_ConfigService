989c636627fe7e503c94b03548fd1325d7b781a1
Todd Miller <tmiller@fairwaytech.com>
2017-02-01 09:00:59 -0800

    Add the mysql driver to the startup script

--- service/src/main/docker/docker-startup.sh
+++ service/src/main/docker/docker-startup.sh
@@ -11,6 +11,7 @@ java \
     -Dspring.datasource.username="${CONFIG_DB_USER}" \
     -Dspring.datasource.password="${CONFIG_DB_PASSWORD}" \
     -Dspring.datasource.type=com.zaxxer.hikari.HikariDataSource \
+    -Dspring.datasource.driver-class-name=com.mysql.jdbc.Driver \
     -jar /tds-config-service.jar \
     --server-port="8080" \
     --server.undertow.buffer-size=16384 \
