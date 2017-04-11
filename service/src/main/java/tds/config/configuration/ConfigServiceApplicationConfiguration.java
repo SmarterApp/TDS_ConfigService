9dcdc4e632723f4fcc1f200c24c13df71fdb8c6d
wtritch <wtritchler@fairwaytech.com>
2017-04-11 16:30:10 -0700

    Bump TDS_Common dependency version and optionally enable redis caching

--- service/src/main/java/tds/config/configuration/ConfigServiceApplicationConfiguration.java
+++ service/src/main/java/tds/config/configuration/ConfigServiceApplicationConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Import;
 
 import tds.common.configuration.CacheConfiguration;
 import tds.common.configuration.DataSourceConfiguration;
+import tds.common.configuration.RedisClusterConfiguration;
 import tds.common.configuration.RestTemplateConfiguration;
 import tds.common.configuration.SecurityConfiguration;
 import tds.common.web.advice.ExceptionAdvice;
@@ -17,6 +18,7 @@ import tds.common.web.advice.ExceptionAdvice;
     ExceptionAdvice.class,
     RestTemplateConfiguration.class,
     DataSourceConfiguration.class,
+    RedisClusterConfiguration.class,
     CacheConfiguration.class,
     SecurityConfiguration.class
 })
